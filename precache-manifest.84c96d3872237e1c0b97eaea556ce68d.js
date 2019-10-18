30d64f2ad7dd39aa5b02ed306fbc2113ce7609c1
Illia Hromov <i.hromov@stollerukraine.com>
2019-10-18 17:26:18 +0300

    Updates

---
+++ precache-manifest.84c96d3872237e1c0b97eaea556ce68d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddb595af4764503b6d65f92e5f6a8ba",
+    "url": "/TodoList.io/index.html"
+  },
+  {
+    "revision": "1a333e221afa1a4cc0e2",
+    "url": "/TodoList.io/static/css/main.0b6d28fb.chunk.css"
+  },
+  {
+    "revision": "ec75c153cfe8400032c4",
+    "url": "/TodoList.io/static/js/2.cd279d5e.chunk.js"
+  },
+  {
+    "revision": "1a333e221afa1a4cc0e2",
+    "url": "/TodoList.io/static/js/main.ab43c8fa.chunk.js"
+  },
+  {
+    "revision": "70e48d4a31e54329d8aa",
+    "url": "/TodoList.io/static/js/runtime-main.e73ef607.js"
+  }
+]);
